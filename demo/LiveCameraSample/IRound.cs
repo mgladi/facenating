53f478b9e40b55819cce48642dc6676bd344775c
alonkol <alonkollmann@gmail.com>
2017-07-24 20:08:37 +0300

    Initial Round interface and class

---
+++ demo/LiveCameraSample/IRound.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveCameraSample
+{
+    interface IRound
+    {
+        MainWindow.AppMode[] GetAppModes();
+        string GetRoundDescription();
+        List<int> ComputeFrameScorePerPlayer(LiveCameraResult result);
+    }
+}
---
+++ demo/LiveCameraSample/RoundEmotion.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Common.Contract;
+
+namespace LiveCameraSample
+{
+    enum EmotionType
+    {
+        Anger,
+        Contempt,
+        Disgust,
+        Fear,
+        Happiness,
+        Neutral,
+        Sadness,
+        Surprise,
+    }
+    class RoundEmotion: IRound
+    {
+        public RoundEmotion(EmotionType emotionType)
+        {
+            this.targetEmotion = emotionType;
+        }
+
+        private EmotionType targetEmotion;
+        public MainWindow.AppMode[] GetAppModes()
+        {
+            MainWindow.AppMode[] modes = {MainWindow.AppMode.Emotions};
+            return modes;
+        }
+
+        public string GetRoundDescription()
+        {
+            return $"Try to get the highest '{this.targetEmotion}' score you can!";
+        }
+
+        public List<int> ComputeFrameScorePerPlayer(LiveCameraResult apiResult)
+        {
+            List<int> frameScores = new List<int>();
+            int currScore;
+            KeyValuePair<string, float> currDominantEmotion;
+            foreach (EmotionScores userEmotionScores in apiResult.EmotionScores)
+            {
+                currDominantEmotion = getDominantEmotion(userEmotionScores);
+                currScore = currDominantEmotion.Key == this.targetEmotion.ToString() ? 10 : 0;
+                frameScores.Add(currScore);
+            }
+
+            return frameScores;
+        }
+
+        private KeyValuePair<string, float> getDominantEmotion(EmotionScores scores)
+        {
+            return scores.ToRankedList().Max();
+        }
+
+        private float getRelevantEmotionScoreFromScores(EmotionScores scores, EmotionType emotionType)
+        {
+            switch (emotionType)
+            {
+                case EmotionType.Anger:
+                    return scores.Anger;
+                case EmotionType.Contempt:
+                    return scores.Contempt;
+                case EmotionType.Disgust:
+                    return scores.Disgust;
+                case EmotionType.Fear:
+                    return scores.Fear;
+                case EmotionType.Happiness:
+                    return scores.Happiness;
+                case EmotionType.Neutral:
+                    return scores.Neutral;
+                case EmotionType.Sadness:
+                    return scores.Sadness;
+                case EmotionType.Surprise:
+                    return scores.Surprise;
+            }
+            return 0;
+        }
+    }
+}
