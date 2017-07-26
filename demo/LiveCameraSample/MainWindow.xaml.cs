d77bac6771e5cb4517c9a029f3af0517322be22e
alonkol <alonkollmann@gmail.com>
2017-07-26 17:42:49 +0300

    background music repeat

--- demo/LiveCameraSample/MainWindow.xaml.cs
+++ demo/LiveCameraSample/MainWindow.xaml.cs
@@ -95,7 +95,6 @@ namespace LiveCameraSample
         public enum AppMode
         {
             Participants,
-
             Faces,
             Emotions,
             EmotionsWithClientFaceDetect,
@@ -126,6 +125,10 @@ namespace LiveCameraSample
             StartTimer();
             this.backgroundMusic = SoundProvider.Ukulele;
             this.backgroundMusic.Volume = 0.05;
+            this.backgroundMusic.MediaEnded += new EventHandler((object sender, EventArgs e) => {
+                this.backgroundMusic.Position = TimeSpan.Zero;
+                this.backgroundMusic.Play();
+            });
             this.backgroundMusic.Play();
             t.Elapsed += T_Elapsed;
 
